40eca74b0648e66a39b51b2f2cdb353a7f46c0fa
nOctaveLay <wjddkdml1124@naver.com>
2021-12-15 15:55:23 +0900

    c++로 다시 품, python은 순차적으로 출력해주는데, c++은 다시 한 번 정리필.

---
+++ 자료구조/1406.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    stack<char> front_result;
+    stack<char> end_result;
+    stack<char> result_stack;
+    string input_string;
+    int num_of_operation;
+    char operation;
+
+    cin >> input_string;
+        
+    // 초기 조건
+    for (int i = 0; i < input_string.length(); i++){
+        front_result.push(input_string[i]);
+    }
+
+    cin >> num_of_operation;
+
+    for (num_of_operation; num_of_operation > 0; num_of_operation--) {
+
+        cin >> operation;
+
+        switch (operation){
+            // 커서를 왼쪽으로 한 칸 옮김 (커서가 맨 앞이면 무시됨)
+            case 'L':
+                if (front_result.size() != 0){
+                    end_result.push(front_result.top());
+                    front_result.pop();
+                }
+                break;
+
+            // 커서를 오른쪽으로 한 칸 옮김 (커서가 맨 뒤면 무시됨)
+            case 'D':
+                if (end_result.size() != 0){
+                    front_result.push(end_result.top());
+                    end_result.pop();
+                }
+                break;
+            // 커서 왼쪽에 있는 문자를 삭제함
+            case 'B':
+                if (front_result.size() != 0){
+                    front_result.pop();
+                }
+                break;
+
+            // 커서 왼쪽에 문자를 더함
+            case 'P':
+                char new_char;
+                cin >> new_char;
+                front_result.push(new_char);
+                break;
+        }
+    }
+
+    while (!front_result.empty()){
+        end_result.push(front_result.top());
+        front_result.pop();
+    }
+
+    while (!end_result.empty()) {
+        cout << end_result.top();
+        end_result.pop();        
+    }
+    return 0;
+    
+    
+}
