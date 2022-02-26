a270e1e8483f06db5a66d6310057dcf27608038a
nOctaveLay <wjddkdml1124@naver.com>
2022-02-27 03:59:29 +0900

    리스트 컴프리헨션으로 바뀌었을 때 훨씬 속도가 빨라 이리 만들었다.

--- No Tag/분할 정복 알고리즘/10830.py
+++ No Tag/분할 정복 알고리즘/10830.py
@@ -1,33 +1,11 @@
 # 행렬 제곱
-from unittest import result
-
 # n의 크기를 갖는 항등 행렬을 return 한다.
 def identity(n):
-    result_list = list()
-    for i in range(n):
-        j_list = list()
-        for j in range(n):
-            if i == j:
-                j_list.append(1)
-            else:
-                j_list.append(0)
-        result_list.append(j_list)
-    return result_list
+    return [[1 if i==j else 0 for i in range(n)] for j in range(n)]
 
 # matrix끼리의 곱을 구해서 return해준다.
 def multiply_matrix(matrix_A,matrix_B):
-    # Base Case
-    if len(matrix_A[0]) != len(matrix_B):
-        return -1
-    else:
-        m,n,l = len(matrix_A),len(matrix_A[0]),len(matrix_B)
-        result = [[0] * l for _ in range(m)]
-        for i in range(m):
-            for k in range(l):            
-                for j in range(n):
-                    result[i][k] += (matrix_A[i][j] * matrix_B[j][k]) % 1000
-                result[i][k] %= 1000
-        return result
+    return [[sum(matrix_A[i][k] * matrix_B[k][j] for k in range(len(matrix_A))) % 1000 for j in range(len(matrix_B[0]))] for i in range(len(matrix_A))]
 
 # (matrix_A)^b을 구한다.
 def pow_matrix(matrix_A,b):
