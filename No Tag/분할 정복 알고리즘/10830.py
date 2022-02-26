37ec03283acc2c9a9a7b7ff8fba9482dc33f47e3
nOctaveLay <wjddkdml1124@naver.com>
2022-02-27 04:05:09 +0900

    의미를 알아보기 쉽게 바꾸었다.

--- No Tag/분할 정복 알고리즘/10830.py
+++ No Tag/분할 정복 알고리즘/10830.py
@@ -4,18 +4,20 @@ def identity(n):
     return [[1 if i==j else 0 for i in range(n)] for j in range(n)]
 
 # matrix끼리의 곱을 구해서 return해준다.
-def multiply_matrix(matrix_A,matrix_B):
-    return [[sum(matrix_A[i][k] * matrix_B[k][j] for k in range(len(matrix_A))) % 1000 for j in range(len(matrix_B[0]))] for i in range(len(matrix_A))]
+def multiply_matrix_mod_1000(matrix1,matrix2):
+    return [[sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix1))) % 1000 for j in range(len(matrix2[0]))] for i in range(len(matrix1))]
 
-# (matrix_A)^b을 구한다.
-def pow_matrix(matrix_A,b):
+# (matrix)^b을 구한다.
+def pow_matrix(matrix,b):
+
     # Base Case
-    if b == 0 : return identity(len(matrix_A))
+    if b == 0 : return identity(len(matrix))
+
     # Divide Case
-    if b % 2 == 1 : return multiply_matrix(matrix_A,pow_matrix(matrix_A,b-1))
+    if b % 2 == 1 : return multiply_matrix_mod_1000(matrix,pow_matrix(matrix,b-1))
     else:
-        half_matrix = pow_matrix(matrix_A,b//2)
-        return multiply_matrix(half_matrix,half_matrix)
+        half_matrix = pow_matrix(matrix,b//2)
+        return multiply_matrix_mod_1000(half_matrix,half_matrix)
                     
 if __name__ == "__main__":
     n,b = map(int,input().split())
