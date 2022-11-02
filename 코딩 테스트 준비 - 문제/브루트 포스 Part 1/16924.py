985e898af1a0701b4bf880fc4fc5778b0889f6f7
nOctaveLay <wjddkdml1124@naver.com>
2022-11-02 11:11:51 +0900

    Update 16924.py

--- 코딩 테스트 준비 - 문제/브루트 포스 Part 1/16924.py
+++ 코딩 테스트 준비 - 문제/브루트 포스 Part 1/16924.py
@@ -1,31 +1,48 @@
+
+# concept는 맞았는데, 너무 어렵게 풀어나간문제.
 import sys
 input = sys.stdin.readline
-def IsCross(sx,sy,arr):
-    dx = [-1,1,0,0]
-    dy = [0,0,1,-1]
-    
-    counter = 0
-    # 중심 좌표에서 상하좌우를 확인한 뒤, d를 늘려서 상하좌우를 확인한다.
-    while True:
-        for i in range(4):
-            nx,ny = sx + dx[i]*counter, sy + dy[i]*counter
-            if not (0<=nx<n) or not(0<=ny<m): continue
-            if arr[nx][ny] == '.':
-                return counter-1
-        if counter > max(n,m): return counter-1
-        counter += 1
 
-if __name__ == "__main__":
-    n, m = map(int,input().split())
-    arr = [list(input().rstrip("\n")) for _ in range(n)]
-    result = []
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] == '*':
-                k = IsCross(i,j,arr)
-                if k:
-                    for l in range(1,k+1):
-                        result.append([i+1,j+1,l])
-                        
-    for elem in result:
+h, w = map(int, input().split())
+arr = [input().rstrip() for _ in range(h)]
+check = [[0 for _ in range(w)] for _ in range(h)]
+ans = [] # x, y, s
+
+dr = [0, 0, 1, -1]
+dc = [1, -1, 0, 0]
+
+def checks(r, c):
+    for s in range(1, w):
+        for i in range(4):
+            nr = r + dr[i] * s
+            nc = c + dc[i] * s
+            if not (0 <= nr < h and 0 <= nc < w and arr[nr][nc] == '*'):
+                return
+        ans.append([r + 1, c + 1, s])
+        for i in range(4):
+            nr = r + dr[i] * s
+            nc = c + dc[i] * s
+            check[nr][nc] = 0
+        check[r][c] = 0
+
+for i in range(h):
+    for j in range(w):
+        if arr[i][j] == '*':
+            check[i][j] = 1
+
+for i in range(h):
+    for j in range(w):
+        if arr[i][j] == '*':
+            checks(i, j)
+
+total = 0
+for i in range(h):
+    for j in range(w):
+        total += check[i][j]
+
+if total == 0:
+    print(len(ans))
+    for elem in ans:
         print(*elem)
+else:
+    print(-1)
