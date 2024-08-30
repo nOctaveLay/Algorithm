51ffab06e062c8fbe14cc8eecdd30a7e1880a633
nOctaveLay <wjddkdml1124@naver.com>
2024-08-30 23:00:36 +0900

    merge conflict 해결하다가 다시 처리하는 중... 데이터 유실됬을 수도 있음... 그러면 버전 관리로 혼내줄거야...

--- Baekjoon/알고리즘 기초 2/알고리즘 기초 510 - 브루트 포스 (N과 M)/15665.py
+++
@@ -1,5 +0,0 @@
-from itertools import product
-n, m = map(int,input().split())
-a = sorted(list(product(set(map(int,input().split())),repeat = m)))
-for i in a:
-    print(*i)
--- Baekjoon/코딩 테스트 준비 문제/시뮬레이션과 구현/1913.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-input = sys.stdin.readline
-n = int(input())
-
-center = n//2
-direction = [(-1,0),(0,1),(1,0)]
