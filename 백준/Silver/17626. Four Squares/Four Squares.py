bf6f76c009002c5103f155fcba79f119f369893d
nOctaveLay <wjddkdml1124@naver.com>
2024-12-26 10:42:36 +0900

    [Silver III] Title: Four Squares, Time: 124 ms, Memory: 111092 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,10 +1,7 @@
 import math
 n=int(input())
 dp=[5 for _ in range(n+1)]
-# dp[3] = min(dp[2] + dp[1]) 
-# dp[4] = min(dp[3] + dp[1], dp[2] + dp[2])
-# dp[n] = min(dp[n-1] + dp[1], dp[n-2]+dp[2], ...)
-
+# dp[n] = min(dp[n-k**2] + dp[k])
 # 특이사항 : dp[k**2] = 1
 
 for i in range(1, int(math.sqrt(n))+1):
@@ -12,5 +9,5 @@ for i in range(1, int(math.sqrt(n))+1):
 
 for i in range(2, n+1):
     for j in range(1, int(math.sqrt(i))+1):
-        dp[i] = min(dp[i], dp[i-j**2]+dp[j**2])
+        dp[i] = min(dp[i], dp[i-j*j]+dp[j*j])
 print(dp[n])
