29727ab3af846cd966a73db3787db7c96e5d8423
nOctaveLay <wjddkdml1124@naver.com>
2025-03-08 00:10:35 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -8,27 +8,18 @@ arr = list(map(int,input().split()))
 result = [0]*n
 
 for index, num_of_left in enumerate(arr):
-    # print(index)
     zero_count = 0
     for position in range(n):
-        # print(zero_count, position, num_of_left)
         # 왼쪽에 키 큰 사람의 조건을 만족할 경우
         if zero_count == num_of_left:
             if result[position] == 0:
                 result[position] = index + 1
                 break
-            # result[position] = index+1
+            
         # 조건을 만족하지 못할 경우
 
         if result[position] == 0:
             zero_count += 1
     
 
-    # if result[position] == 0:
-    #     result[position] = index+1
-    # else:
-    #     while result[position] != 0:
-    #         position += 1
-    #     result[position] = index+1
-    # print(result)
 print(*result)
