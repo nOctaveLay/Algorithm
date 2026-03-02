a592208ac74ae03c3b1ad8c92909ed12c0d9380d
nOctaveLay <wjddkdml1124@naver.com>
2026-03-02 19:00:07 +0900

    [Bronze III] Title: 할로윈의 사탕, Time: 84 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/10178. 할로윈의 사탕/할로윈의 사탕.py
@@ -0,0 +1,8 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+for _ in range(n):
+    a,b = map(int,input().split())
+    print(f"You get {a//b} piece(s) and your dad gets {a%b} piece(s).")
